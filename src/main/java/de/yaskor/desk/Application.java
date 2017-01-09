9ca817cc1bf4cd63c3fa7a2fe7331b3ea1f5da75
saka <saka@prehcmservices.de>
2017-01-09 11:51:25 +0100

    context

--- src/main/java/de/yaskor/desk/Application.java
+++ src/main/java/de/yaskor/desk/Application.java
@@ -75,7 +75,7 @@ public class Application extends SpringBootServletInitializer {
 
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tesc = new TomcatEmbeddedServletContainerFactory("/desk", 80);
+        TomcatEmbeddedServletContainerFactory tesc = new TomcatEmbeddedServletContainerFactory("", 8080);
         return tesc;
     }
 }
